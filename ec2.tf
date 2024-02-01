8ca74c047685ae0def3baa9729e88cb2705d8e94
AnoohhyaChoudavaram <108270716+AnoohhyaChoudavaram@users.noreply.github.com>
2024-02-01 11:16:48 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "MyFirstEc2instance_from_terraform" {
   ami = "ami-0759f51a90924c166"
   instance_type = "t2.micro"
   tags = {
-    Name = "Ec2started_from_console_from_terraform"
+    Name = "EC2_public"
   }
   key_name = "linux"
   user_data = <<-EOF
